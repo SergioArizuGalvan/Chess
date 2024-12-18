cb35a1d34a507be29090482da47656c409e1a4ff
Sergio Arizu Galvan <sergio.arizu@autonoma.cat>
2024-12-18 12:40:03 +0100

    Turnos y mal movimineto de piezas

--- chess.py
+++ chess.py
@@ -15,3 +15,28 @@ def printar_tablero(tablero):
         print(" ".join(columna))
 
 printar_tablero(tablero_fun())
+
+
+
+def turno(player):
+    if player == "black":
+        return "white"
+    else:
+        return "black"
+
+
+def tus_piezas(player, pos_inicio, pos_final, tablero):
+    x_ini, y_ini = pos_inicio
+    x_fin, y_fin = pos_final
+    pieza=tablero[x_ini][y_ini]
+
+    if pieza == ".":
+        return "NO HAY NINGUNA PIEZA"
+
+    if (pieza.isupper() and player != "white") or (pieza.islowe() and player != "black"):
+        return "NO ES TU TURNO"
+
+    # DE MOMENTO TODO PERMITIDO, hay que programar los movimientos de cada pieza
+    tablero[x_ini][y_ini]="."
+    tablero[x_fin][y_fin]=pieza
+    return "MOVIMIENTO EXITOSO"
